0d25aa917aee44a35a4f81aa200561d49e919630
wupeng <ban-ming@foxmail.com>
2023-06-01 00:08:10 +0800

    add debug draw

--- src/apps/debug_draw_sample/debug_draw_sample.cpp
+++ src/apps/debug_draw_sample/debug_draw_sample.cpp
@@ -2,37 +2,56 @@
 
 #include <debugdraw/debugdraw.h>
 
-void DebugDrawSample::onInit() { ddInit(); }
-
-void DebugDrawSample::onShutdown() { ddShutdown(); }
-
-void DebugDrawSample::onUpdate(float _deltaTime) {
-  const bx::Vec3 at = {0.0f, 0.0f, 0.0f};
-  const bx::Vec3 eye = {0.0f, 0.0f, -35.0f};
-
-  // Set view and projection matrix for view 0.
-  {
-    float view[16];
-    bx::mtxLookAt(view, eye, at);
-
-    float proj[16];
-    bx::mtxProj(proj, 60.0f, float(m_width) / float(m_height), 0.1f, 100.0f,
-                bgfx::getCaps()->homogeneousDepth);
-    bgfx::setViewTransform(0, view, proj);
-  }
-}
-static DdVertex s_bunnyVertices[] = {{25.0883f, -44.2788f, 31.0055f},
-                                     {0.945623f, 53.5504f, -24.6146f},
-                                     {-0.94455f, -14.3443f, -16.8223f}};
-
-void DebugDrawSample::onRender(float _inAspectRatio) {
-  DebugDrawEncoder dde;
-
-  dde.begin(0);
-  dde.drawAxis(0.0f, 0.0f, 0.0f);
-
-//   dde.drawLineList()
-   dde.end();
+void DebugDrawSample::onInit()
+{
+    ddInit();
 }
 
-void DebugDrawSample::onGui() {}
+void DebugDrawSample::onShutdown()
+{
+    ddShutdown();
+}
+
+void DebugDrawSample::onUpdate(float _deltaTime)
+{
+    const bx::Vec3 at = {0.0f, 0.0f, 0.0f};
+    const bx::Vec3 eye = {0.0f, 0.0f, -35.0f};
+
+    // Set view and projection matrix for view 0.
+    {
+        float view[16];
+        bx::mtxLookAt(view, eye, at);
+
+        float proj[16];
+        bx::mtxProj(proj, 60.0f, float(m_width) / float(m_height), 0.1f, 100.0f, bgfx::getCaps()->homogeneousDepth);
+        bgfx::setViewTransform(0, view, proj);
+    }
+}
+static DdVertex s_linesVertices[] = {{0, 0, 0},
+                                     {1, 3, 0},
+                                     {2, 3, 0},
+                                     {4, 3, 0}};
+
+static const uint16_t s_linesIndices[] = {0, 1, 2, 3};
+
+void DebugDrawSample::onRender(float _inAspectRatio)
+{
+    DebugDrawEncoder dde;
+
+    dde.begin(0);
+    dde.drawAxis(0.0f, 0.0f, 0.0f);
+
+    dde.drawOrb(5, 5, 0, 2);
+    dde.drawCone({10, 10, 0}, {12, 12, 0}, 2);
+    dde.setColor(0xfff0c0ff);
+    dde.drawCapsule({-5, -5, 0}, {-5, -5, 0}, 3);
+    // dde.push();
+    // dde.setStipple(false, 4);
+    dde.drawLineList(4, s_linesVertices, 4, s_linesIndices);
+    // dde.pop();
+    dde.end();
+}
+
+void DebugDrawSample::onGui()
+{
+}
